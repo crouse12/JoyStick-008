d806d015d7c7258b2860f385eb0565c2a9f1cf96
crouse12 <crousekimo@yahoo.com.tw>
2018-10-05 11:08:51 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,26 +16,27 @@ namespace JoyStick {
         return pins.digitalReadPin(DigitalPin.P15)
     }
         //% blockId="d" block="Button D" 
-    //% weight=5
+    //% weight=4
     export function d(): number {
         return pins.digitalReadPin(DigitalPin.P14)
     }
         //% blockId="e" block="Button E" 
-    //% weight=6
+    //% weight=4
     export function e(): number {
         return pins.digitalReadPin(DigitalPin.P13)
     }
         //% blockId="f" block="Button F" 
-    //% weight=7
+    //% weight=4
     export function f(): number {
         return pins.digitalReadPin(DigitalPin.P12)
     }
-            //% blockId="a" block="Button A" weight=2
+                //% blockId="a" block="Button A" 
+            //% weight=5
     export function a(): number {
         return pins.digitalReadPin(DigitalPin.P5)
     }
             //% blockId="b" block="Button B" 
-            //% weight=3
+            //% weight=5
     export function b(): number {
         return pins.digitalReadPin(DigitalPin.P11)
     }
