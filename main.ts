1054cc75ba3b377824e3a50c427b3da4f2777dea
crouse12 <crousekimo@yahoo.com.tw>
2018-10-05 10:31:23 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,38 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="JoyStick"
 namespace JoyStick {
-    //% blockId="x" block="X axis"
+    //% blockId="x" block="X axis" weight=0
     export function x(): number {
         return pins.analogReadPin(AnalogPin.P2)
     }
-    //% blockId="y" block="Y axis"
+    //% blockId="y" block="Y axis" weight=1
     export function y(): number {
         return pins.analogReadPin(AnalogPin.P1)
     }
-        //% blockId="c" block="Button C"
+        //% blockId="c" block="Button C" weight=4
     export function c(): number {
         return pins.digitalReadPin(DigitalPin.P15)
     }
-        //% blockId="d" block="Button D"
+        //% blockId="d" block="Button D" weight=5
     export function d(): number {
         return pins.digitalReadPin(DigitalPin.P14)
     }
-        //% blockId="e" block="Button E"
+        //% blockId="e" block="Button E" weight=6
     export function e(): number {
         return pins.digitalReadPin(DigitalPin.P13)
     }
-        //% blockId="f" block="Button F"
+        //% blockId="f" block="Button F" weight=7
     export function f(): number {
         return pins.digitalReadPin(DigitalPin.P12)
     }
-            //% blockId="a" block="Button A"
+            //% blockId="a" block="Button A" weight=2
     export function a(): number {
         return pins.digitalReadPin(DigitalPin.P5)
     }
-            //% blockId="b" block="Button B"
+            //% blockId="b" block="Button B" weight=3
     export function b(): number {
         return pins.digitalReadPin(DigitalPin.P11)
     }
-    //% blockId="motor" block="Start vibration motor %value"
+    //% blockId="motor" block="Start vibration motor %value" weight=8
     export function motor(value: number) {
        pins.digitalWritePin(DigitalPin.P16, value)
     }
