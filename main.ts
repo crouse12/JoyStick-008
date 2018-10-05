52f8270490afc3f6d35614200c4290ee9de90cfa
crouse12 <crousekimo@yahoo.com.tw>
2018-10-05 09:39:32 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
-    //% blockId="x" block="x"
+    //% blockId="x" block="x axis"
     export function x(): number {
         return pins.analogReadPin(AnalogPin.P2)
     }
-    //% blockId="y" block="Y"
+    //% blockId="y" block="Y axis"
     export function y(): number {
         return pins.analogReadPin(AnalogPin.P1)
     }
-        //% blockId="c" block="C"
+        //% blockId="c" block="Button C"
     export function c(): number {
         return pins.digitalReadPin(DigitalPin.P15)
     }
-        //% blockId="d" block="D"
+        //% blockId="d" block="Button D"
     export function d(): number {
         return pins.digitalReadPin(DigitalPin.P14)
     }
-        //% blockId="e" block="E"
+        //% blockId="e" block="Button E"
     export function e(): number {
         return pins.digitalReadPin(DigitalPin.P13)
     }
-        //% blockId="f" block="F"
+        //% blockId="f" block="Button F"
     export function f(): number {
         return pins.digitalReadPin(DigitalPin.P12)
     }
