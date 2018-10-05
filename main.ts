53b158e6b83e8c71b778f258096a742c69a6f17a
crouse12 <crousekimo@yahoo.com.tw>
2018-10-05 08:39:58 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
-    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
+    //% blockId="areaOfRectangle" block="area of rectangle length"
     export function areaOfRectangle(length: number, width:number): number {
         return length*width
     }
