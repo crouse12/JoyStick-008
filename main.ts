07639c562afcb8c10476c1a1fc00ea909bdb66e5
crouse12 <crousekimo@yahoo.com.tw>
2018-10-05 09:32:16 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,26 @@
 namespace tools {
     //% blockId="x" block="x"
     export function x(): number {
-        return pins.analogReadPin(AnalogPin.P2);
+        return pins.analogReadPin(AnalogPin.P2)
     }
     //% blockId="y" block="Y"
     export function y(): number {
-        return pins.analogReadPin(AnalogPin.P1);
+        return pins.analogReadPin(AnalogPin.P1)
+    }
+        //% blockId="c" block="C"
+    export function c(): number {
+        return pins.digitalReadPin(DigitalPin.P15)
+    }
+        //% blockId="d" block="D"
+    export function d(): number {
+        return pins.digitalReadPin(DigitalPin.P14)
+    }
+        //% blockId="e" block="E"
+    export function e(): number {
+        return pins.digitalReadPin(DigitalPin.P13)
+    }
+        //% blockId="f" block="F"
+    export function f(): number {
+        return pins.digitalReadPin(DigitalPin.P12)
     }
 }
