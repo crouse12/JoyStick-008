b7f18e11691314f0bb2fa2b402ad1ce51a8c4a86
crouse12 <crousekimo@yahoo.com.tw>
2018-10-05 08:36:03 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
-    /**
-    * 計算長方形面積，並回傳
-    */
-    //% blockId="areaOfRectangle" block="area of rectangle length"
+    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
+    //% blockGap=2 weight=0 blockExternalInputs=true
     export function areaOfRectangle(length: number, width:number): number {
         return length*width
     }
