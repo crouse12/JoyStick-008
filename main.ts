c03f43b048f5572bd7b0fa86009937ad0df9072d
crouse12 <crousekimo@yahoo.com.tw>
2018-10-05 08:54:50 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
-    //% blockId="areaOfRectangle" block="area of rectangle length"
-    export function areaOfRectangle(length: number, width:number): number {
-        return length*width
+    //% blockId="x" block="x"
+    export function x(): number {
+        return pins.analogReadPin(AnalogPin.P2);
+    }
+    //% blockId="y" block="Y"
+    export function y(): number {
+        return pins.analogReadPin(AnalogPin.P1);
     }
 }
